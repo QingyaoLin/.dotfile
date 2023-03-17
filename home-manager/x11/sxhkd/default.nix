f29e63d6890bb09cc621701210a8df24d9f204f7
Qingyao Lin <83944508+QingyaoLin@users.noreply.github.com>
2023-03-17 08:18:07 +0800

    Update default.nix

--- home-manager/x11/sxhkd/default.nix
+++ home-manager/x11/sxhkd/default.nix
@@ -4,56 +4,185 @@
   config = lib.mkIf (config.xsession.enable) {
     services.sxhkd = {
       enable = true;
-      keybindings = {
-        "super + Return" = "alacritty";
-        "alt + @space" = "rofi -show drun";
-        # "Super_L; @Super_L" = "eww";
-        "super + o ; {b,t}" = "{microsoft-edge,alacritty --class FloatAlacritty}";
-        "super + alt + q" = "bspc quit";
-        "super + {_, shift + }w" = "bspc node -{c, k}";
-        "alt + {_,shift + }f" = "bspc node -t {floating,fullscreen} || bspc node -t tiled";
-        "alt + m" = "bspc desktop -l next";
-        "super + {h,j,k,l}" = "bspc node -f {west,south,north,east}";
-        "alt + {_,shift + }Tab" = "bspc desktop -f {next,prev}.local";
-        "super + {grave,Tab}" = "bspc {node,desktop} -f last";
-        "super + {1-5}" = "bspc desktop -f '^{1-5}'";
-        "super + g" = "bspc node -s biggest.window";
-        "alt + s : {h,j,k,l}" = "bspc node -s {west,south,north,east}";
-        "super + shift + {1-5}" = "bspc node -d '^{1-5}' --follow";
-        "alt + p : {h,j,k,l}" = "bspc node -p {west,south,north,east}";
-        "alt + shift + space" = "bspc node -p cancel";
-        "super + shift + {Left,Down,Up,Right}" = "bspc node -v {-20 0,0 20,0 -20,20 0}";
-      };
       extraConfig =
       ''
+# using `nix-shell -p xorg.xev --run "xev -event keyboard"` will run xev,
+# with filtering for keyboard events.
+
+
+#
+# ============ WM independent hotkeys =============
+#
+
+# terminal emulator
+super + Return
+	alacritty
+
+# program launcher
+alt + @space
+	rofi -show drun 
+
+# sxhkd、bspwm、dunst restart
+super + Escape
+	pkill -USR1 -x sxhkd; \
+	bspc wm -r; \
+	sleep 1; \
+	dunstify "Reload all configuration." -u low
+
+# Show sxhkd keyboard help
+alt + @slash
+  "$HOME"/.config/script/sxhkd-help.sh
+
+# opens the Start menu
+# Super_L; @Super_L
+#	  eww
+
+# open browser/vscode/float_terminal
+super + o ; {b,t}
+	{microsoft-edge,alacritty --class FloatAlacritty}
+
+
+#
+# bspwm hotkeys
+#
+
+# quit bspwm
+super + alt + q
+	bspc quit
+
+# close and kill
+super + {_,shift + }w
+	bspc node -{c,k}
+
+
+#
+# state/flags
+#
+
+# set the window state
+alt + {_,shift + }f
+	bspc node -t {floating,fullscreen} || bspc node -t tiled
+
+# alternate between the tiled and monocle layout
+alt + m
+	bspc desktop -l next
+
+# unhide/hide window
+alt + h
+  "$HOME"/.config/sxhkd/hidden.sh
+
+
+#
+# focus
+#
+
+# focus the node in the given direction
+super + {h,j,k,l}
+	bspc node -f {west,south,north,east}
+
+# focus the next/previous desktop
+alt + {_,shift + }Tab
+	bspc desktop -f {next,prev}.local
+
+# focus the last node/desktop
+super + {grave,Tab}
+	bspc {node,desktop} -f last
+
+# focus to the given desktop
+super + {1-5}
+	bspc desktop -f '^{1-5}'
+
+
+#
+# swap
+#
+
+# swap the current node and the biggest window
+super + g
+	bspc node -s biggest.window
+
+# swap window position
+alt + s : {h,j,k,l}
+  bspc node -s {west,south,north,east}
+
+
+#
+# send
+#
+
+# send and fo to the given desktop
+super + shift + {1-5}
+	bspc node -d '^{1-5}' --follow
+
+
+#
+# preselect
+#
+
+# preselect the direction
+alt + p : {h,j,k,l}
+	bspc node -p {west,south,north,east}
+
+# cancel the preselection for the focused node
+alt + shift + space
+	bspc node -p cancel
+
+
+#
+# ============= Resize focused window ==============
+#
+
+# Expand or contract the size of the focused window
 super + r : {h,j,k,l}
   n=10; \
-  { d1=left;   d2=right;  dx=-$n; dy=0; \
-  , d1=bottom; d2=top;    dx=0;   dy=$n; \
+  { d1=left;   d2=right;  dx=-$n; dy=0;   \
+  , d1=bottom; d2=top;    dx=0;   dy=$n;  \
   , d1=top;    d2=bottom; dx=0;   dy=-$n; \
-  , d1=right;  d2=left;   dx=$n;  dy=0; \
+  , d1=right;  d2=left;   dx=$n;  dy=0;   \
   } \
   bspc node --resize $d1 $dx $dy || bspc node --resize $d2 $dx $dy
 
-super + Escape
-  pkill -USR1 -x sxhkd; \
-  bspc wm -r; \
-  sleep 1; \
-  dunstify "Reload all configuration." -u low
 
-alt + h
-  ~/.config/sxhkd/script/hidden.sh
-     
-alt + @slash
-  ~/.config/sxhkd/script/sxhkd-help.sh
+#
+# ============= Move floating window ===============
+#
+
+# move a floating window
+super + shift + {Left,Down,Up,Right}
+  bspc node -v {-20 0,0 20,0 -20,20 0}
+
+
+#
+# =================== Audio volume =================
+#
+
+# Fuling 98 external keyboard
+XF86Tools
+  amixer -q sset Master toggle 
+
+XF86Audio{LowerVolume,RaiseVolume}
+  amixer -q sset Master 2%{-,+}
+
+# Laptop Keyboard
+#XF86AudioMute
+#   amixer -q sset Master toggle
+#XF86audio{LowerVolume,RaiseVolume}
+#   amixer -q sset Master 2%{-,+}
+
+
+#
+# ==================== Brightness ==================
+#
+
+# Fuling 98 external keyboard
+XF86Audio{Mute,Stop}
+  brightnessctl set 5%{-,+}
+
+# Laptop Keyboard
+#XF86MonBrightness{Down,Up}
+#   brightnessctl set 5%{-,+}
+
       '';
-    };
-
-    home.file = {
-      ".config/sxhkd/script" = {
-        recursive = true;
-	source = config.lib.file.mkOutOfStoreSymlink ./script;
-      };
     };
   };
 }
