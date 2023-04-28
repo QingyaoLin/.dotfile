0e9f27a1dfd59fef93c4d48f23590ea7e7d94b5f
Qingyao Lin <83944508+QingyaoLin@users.noreply.github.com>
2023-04-28 11:03:05 +0800

    Update inputmethod.nix

--- home-manager/global/inputmethod.nix
+++ home-manager/global/inputmethod.nix
@@ -4,8 +4,9 @@
   i18n.inputMethod = {
     enabled = "fcitx5";
     fcitx5.addons = with pkgs; [
-      fcitx5-rime
-      fcitx5-chinese-addons
+       fcitx5-rime
+       fcitx5-gtk
+       fcitx5-lua
     ];
   };
 }
