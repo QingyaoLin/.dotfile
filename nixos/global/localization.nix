408529b7b2d764f7bb153ab264688dead0871723
QingyaoLin <2650426778@qq.com>
2023-04-28 10:31:20 +0800

    fix error inputmethod

--- nixos/global/localization.nix
+++ nixos/global/localization.nix
@@ -18,9 +18,6 @@
       fcitx5.addons = with pkgs; [
         fcitx5-rime
         fcitx5-chinese-addons
-        fcitx5-table-extra
-        fcitx5-pinyin-moegirl
-        fcitx5-pinyin-zhwiki
       ];
     };
   };
