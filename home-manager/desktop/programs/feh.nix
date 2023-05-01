5331d37cc0cb63d4dd6f16a9944c0eef77fb42ac
Qingyao Lin <83944508+QingyaoLin@users.noreply.github.com>
2023-05-01 16:23:01 +0800

    Update feh.nix

--- home-manager/desktop/programs/feh.nix
+++ home-manager/desktop/programs/feh.nix
@@ -1,5 +1,42 @@
 { lib, config, pkgs, ... }:
 
+let
+  wallpaper = "/home/lqy/.wallpaper";
+  extraArgs = [];
+in
 {
   programs.feh.enable = true;
+
+  systemd.user.timers.feh = {
+    Unit = {
+      Description = "Run every 1h feh services";
+      Documentation = [ "man:systemd.timers(5)" ];
+      After = [ "graphical-session.target" ];
+      PartOf = [ "bspwm-session.target" ];
+    };
+
+    Timer = {
+      OnCalendar = "*:*:30";
+    };
+
+    Install = {
+      WantedBy = [ "bspwm-session.target" ];
+    };
+  };
+
+  systemd.user.services.feh = {
+    Unit = {
+      Description = "feh desktop wallpaper";
+      Documentation = [ "man:feh" ];
+    };
+
+    Service = {
+      ExecStart = lib.concatStringsSep " " (
+        [
+          "${pkgs.feh}/bin/feh"
+          "--bg-fill --randomize ${wallpaper}"
+        ] ++ extraArgs
+      );
+    };
+  };
 }
