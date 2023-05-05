88c8258b5abc174c22d384894151dc82a23d98ac
QingyaoLin <2650426778@qq.com>
2023-05-05 12:42:23 +0800

    proxy

--- home-manager/global/shell.nix
+++ home-manager/global/shell.nix
@@ -4,8 +4,8 @@
   home.sessionVariables = {
     EDITOR = "nvim";
     VISUAL = "nvim";
-    # http_proxy = "http://127.0.0.1:7890";
-    # https_proxy = "http://127.0.0.1:7890";
+    http_proxy = "http://127.0.0.1:7890";
+    https_proxy = "http://127.0.0.1:7890";
     # all_proxy = "socks5://127.0.0.1:7890";
   };
 
