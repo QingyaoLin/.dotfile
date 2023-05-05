37fa2ea4d7014e4bd89c4c815a3c1d2123de8b69
QingyaoLin <2650426778@qq.com>
2023-05-05 12:44:04 +0800

    change config

--- nixos/global/network.nix
+++ nixos/global/network.nix
@@ -13,11 +13,11 @@
     #   httpsProxy = "http://127.0.0.1:7890";
     #   allProxy = "socks5://127.0.0.1:7890";
     # };
-    # firewall = {
-    #   enable = true;
+    firewall = {
+      enable = false;
     #   allowedTCPPorts = [ 80 443 8080 ];
     #   allowedUDPPorts = [];
-    # };
+    };
   };
   programs.nm-applet.enable = true;
 }
