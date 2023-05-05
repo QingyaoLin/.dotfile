e020f8389ba33de5ee405fb912c976b7244ee931
Qingyao Lin <83944508+QingyaoLin@users.noreply.github.com>
2023-05-05 12:08:13 +0800

    Update sxhkd.nix

--- home-manager/desktop/programs/sxhkd.nix
+++ home-manager/desktop/programs/sxhkd.nix
@@ -6,21 +6,4 @@
   };
   
   xdg.configFile."sxhkd".source = ../../../config/sxhkd;
-  
-  systemd.user.services.sxhkd = {
-    Unit = {
-      Description = "Simple X hotkey daemon";
-      Documentation = [ "man:sxhkd" ];
-      After = [ "graphical-session.target" ];
-      PartOf = [ "bspwm-session.target" ];
-    };
-
-    Service = {
-      ExecStart = "${pkgs.sxhkd}/bin/sxhkd";
-    };
-
-    Install = {
-      WantedBy = [ "bspwm-session.target" ];
-    };
-  };
 }
