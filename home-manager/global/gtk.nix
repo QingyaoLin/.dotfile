97760a7cb944938493a9a83525a705246f6c3fe9
Qingyao Lin <83944508+QingyaoLin@users.noreply.github.com>
2023-05-05 17:32:22 +0800

    Update gtk.nix

--- home-manager/global/gtk.nix
+++ home-manager/global/gtk.nix
@@ -1,15 +1,16 @@
 { lib, config, pkgs, ... }:
 
+let
+  inherit (config.colorScheme) kind;
+in
 {
   gtk.enable = true;
 
   gtk.cursorTheme = {
     package = pkgs.catppuccin-cursors.macchiatoPink;
-    name = "Catppuccin-Macchiato-Pink-Cursors";
+    # gtk-cursor-theme-name: cursor 目录中 index.theme 中的 [icon theme] -> Name
+    name = "Catppuccin-Macchiato-Pink";
     size = 32;
-  };
-  home.file.".local/share/icons/default" = {
-    source = "${pkgs.catppuccin-cursors.macchiatoPink}/share/icons/Catppuccin-Macchiato-Pink-Cursors";
   };
 
   gtk.theme = {
@@ -19,13 +20,20 @@
       tweaks = [ "rimless" "black" ];
       variant = "macchiato";
     };
+    # gtk-theme-name: 目录的名称
     name = "Catppuccin-Macchiato-Compact-Pink-Dark";
   };
 
   gtk.iconTheme = {
     package = pkgs.papirus-icon-theme;
-    name = "Papirus-Dark";
+    # gtk-icon-theme-name: 目录的名称
+    name =
+      if kind == "dark" then
+        "Papirus-Dark"
+      else
+        "Papirus-light";
   };
+
 
   gtk.font = {
     name = "JetBrainsMono Nerd Font";
@@ -39,4 +47,20 @@
   gtk.gtk3 = { };
 
   gtk.gtk4 = { };
+
+  home.file.".local/share/icons/Catppuccin-Macchiato-Pink-Cursors" = {
+    source = "${pkgs.catppuccin-cursors.macchiatoPink}/share/icons/Catppuccin-Macchiato-Pink-Cursors";
+  };
+  home.file.".local/share/themes" = {
+    source = "${pkgs.unstable.catppuccin-gtk}/share/themes";
+  };
+  home.file.".local/share/icons/Papirus" = {
+    source = "${pkgs.papirus-icon-theme}/share/icons/Papirus";
+  };
+  home.file.".local/share/icons/Papirus-Dark" = {
+    source = "${pkgs.papirus-icon-theme}/share/icons/Papirus-Dark";
+  };
+  home.file.".local/share/icons/Papirus-Light" = {
+    source = "${pkgs.papirus-icon-theme}/share/icons/Papirus-Light";
+  };
 }
