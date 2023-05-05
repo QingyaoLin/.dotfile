4c19267c4cadf9281da31735bc542271910feef1
Qingyao Lin <83944508+QingyaoLin@users.noreply.github.com>
2023-05-05 17:33:10 +0800

    Create qt.nix

---
+++ home-manager/global/qt.nix
@@ -0,0 +1,12 @@
+{ lib, config, pkgs, ... }:
+
+{
+  qt = {
+    enable = true;
+    platformTheme = "gtk";
+    style = {
+      name = "gtk2";
+      package = pkgs.libsForQt5.qtstyleplugins;
+    };
+  };
+}
