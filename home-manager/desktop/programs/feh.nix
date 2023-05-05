f924807197b723abee6af544472522932ed1a5db
Qingyao Lin <83944508+QingyaoLin@users.noreply.github.com>
2023-05-05 12:06:39 +0800

    Update feh.nix

--- home-manager/desktop/programs/feh.nix
+++ home-manager/desktop/programs/feh.nix
@@ -3,4 +3,29 @@
 
 {
   programs.feh.enable = true;
+  
+  systemd.user.timers.feh = {
+    Unit = {
+      Description = "Runs wallpaperscript every 10 minutes";
+      PartOf = [ "graphical-session.target" ];
+      After = [ "graphical-session.target" ];
+    };
+    Timer = {
+      OnActiveSec = "0s";
+      OnUnitActiveSec = "10s";
+    };
+    Install = {
+      WantedBy = [ "bspwm-session.target" ];
+    };
+  };
+
+  systemd.user.services.feh = {
+    Unit = {
+      Description = "Change the wallapaper";
+    };
+    Service = {
+      Environment = "DISPLAY=:0";
+      ExecStart = "${pkgs.feh}/bin/feh --no-fehbg --bg-fill --randomize /home/lqy/.wallpaper";
+    };
+  };
 }
